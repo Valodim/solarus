e7b57a212a160b0896d1c7d1c04fdd94e43a722c
Vincent Breitmoser <valodim@mugenguild.com>
2012-05-06 02:08:31 +0200

    editor: add first pieces of map loading to maploader.lua

--- tools/maploader.lua
+++ tools/maploader.lua
@@ -1,1 +1,19 @@
-print("Hello World from Lua!")
+function loadMap(mapId, map)
+    print("Loading " .. mapId .. " :)")
+
+    map_data = require("map_" .. mapId .. "_data")
+
+    -- Set a bunch of trivial data in this map
+    map:test()
+    map:setSize(map_data.width, map_data.height)
+    map:setWorld(map_data.world)
+    map:setFloor(map_data.floor)
+    map:setLocation(map_data.x, map_data.y)
+    map:setSmallKeysVariable(map_data.small_keys_variable)
+    map:setTileset(map_data.tileset)
+    map:setMusic(map_data.music_id)
+
+    -- 0 = no errors. yay :)
+    return 0
+
+end
