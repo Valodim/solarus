67d214a4c9669ebd29e8e49b773bf093171e66bd
Vincent Breitmoser <valodim@mugenguild.com>
2012-05-06 04:35:57 +0200

    editor, maploader.lua: some more documentation

--- tools/maploader.lua
+++ tools/maploader.lua
@@ -1,4 +1,5 @@
 
+-- Get bindings to entity types.
 local entity_types = {}
 for k, v in pairs({
                  [0] = 'Tile',
@@ -24,11 +25,15 @@ for k, v in pairs({
         }) do
     entity_types[k] = luajava.bindClass("org.solarus.editor.entities." .. v)
 end
+-- Also get a binding to Layer.
 local Layer = luajava.bindClass("org.solarus.editor.entities.Layer")
 
+-- This function is called from Map.load_lua() and does all the loading of
+-- maps.
 function loadMap(mapId, map)
     print("Loading " .. mapId .. " :)")
 
+    -- run the map file to get the data
     local map_data = require("map_" .. mapId .. "_data")
 
     -- Set a bunch of trivial data in this map
@@ -40,9 +45,18 @@ function loadMap(mapId, map)
     map:setTileset(map_data.tileset_id)
     map:setMusic(map_data.music_id)
 
+    -- iterate over entities
     for k, e in ipairs(map_data.entities) do
 
         local entity = luajava.new(entity_types[e[1]], map)
+
+        -- at this point, joining the attributes and just using entity:parse
+        -- might be a better option, but I wanted to see how this works out as
+        -- lua code.
+
+        -- NOT A FAN OF THIS METHOD. A constructor for each entity type that
+        -- just takes the right number of parameters would probably be a lot
+        -- faster and cleaner.
 
         entity:setLayer(Layer:get(e[2]))
         entity:setPositionTopLeftImpl(e[3], e[4])
@@ -55,6 +69,7 @@ function loadMap(mapId, map)
             pos = pos+2
         end
 
+        -- Parse a couple of static properties
         if entity:hasName() then
             entity:setName(e[pos]);
             pos = pos+1
@@ -70,7 +85,7 @@ function loadMap(mapId, map)
             pos = pos+1
         end
 
-        -- specific properties
+        -- And put the rest inspecificProperties
         props = entity:getPropertyIterator()
         while props:hasNext() do
             name = props:next()
@@ -78,11 +93,13 @@ function loadMap(mapId, map)
             pos = pos+1
         end
 
-        -- some entities need to know their properties before they can be resized
+        -- Some entities need to know their properties before they can be
+        -- resized. Do that now.
         if entity:isSizeVariable() then
             entity:setSize(e[5], e[6])
         end
 
+        -- Some more setup.
         entity:updateImageDescription()
 
         entity:setPositionInMap(e[3], e[4])
